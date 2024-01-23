fa8497a821f12858e14bf4ce38b56164199b9b4a
Rajan <rajanj448@student.sfit.ac.in>
2024-01-23 12:48:40 +0530

    Building the Header Component

--- app/(back-office)/layout.jsx
+++ app/(back-office)/layout.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const layout = ({children}) => {
   return (
     <div className='flex '>
-        <div className="w-56 min-h-screen bg-slate-900 text-slate-50 ">
+        <div className="w-60 min-h-screen bg-slate-900 text-slate-50 ">
             Sidebar
         </div>
         <main className='w-full bg-slate-100 min-h-screen '>
--- components/dashboard/Header.jsx
+++ components/dashboard/Header.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import {
+  Bell,
+  ChevronDown,
+  History,
+  LayoutGrid,
+  Plus,
+  Settings,
+  User2,
+  Users,
+} from "lucide-react";
+import React from "react";
+import SearchInput from "./SearchInput";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className='bg-slate-300'>
-        <h2>Header</h2>
-    </div>
-  )
-}
+    <div className="bg-gray-100 h-12 flex items-center justify-between px-8 border-b border-slate-200 ">
+      <div className="flex gap-3">
+        <button>
+          <History className="w-6 h-6 " />
+        </button>
+        <SearchInput />
+      </div>
+      <div className="flex items-center gap-3">
+        <div className="pr-2 border-r border-gray-300">
+          <button className="p-1 bg-blue-600 rounded-lg">
+            <Plus className="text-slate-50 h-4 w-4 " />
+          </button>
+        </div>
 
-export default Header
+        <div className="flex border-r border-gray-300 space-x-2  ">
+          <button className="p-1 hover:bg-slate-200 rounded-lg">
+            <Users className="text-slate-900 h-4 w-4 " />
+          </button>
+
+          <button className="p-1 hover:bg-slate-200 rounded-lg">
+            <Bell className="text-slate-900 h-4 w-4 " />
+          </button>
+
+          <button className="p-1 hover:bg-slate-200 rounded-lg">
+            <Settings className="text-slate-900 h-4 w-4 " />
+          </button>
+        </div>
+        <div className="flex gap-3">
+          <button className="flex items-center">
+            <span>Mumbai</span>
+            <ChevronDown className="h-4 w-4" />
+          </button>
+          <button>
+            <Image
+              src={"/user.jpg"}
+              alt="user-image"
+              width={96}
+              height={96}
+              className="rounded-full h-8 w-8 border border-slate-800"
+            />
+          </button>
+
+          <button className="flex items-center">
+           
+            <LayoutGrid className="h-6 w-6 text-slate-900" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
