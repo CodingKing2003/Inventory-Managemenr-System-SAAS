55e1634ca9688f56060c936a8afb9a811cfc16a7
Rajan <rajanj448@student.sfit.ac.in>
2024-01-23 17:26:50 +0530

    Adding Icons

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -1,38 +1,72 @@
-import { ChevronLeft,  Home,  ShoppingCart } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import {
+  BaggageClaim,
+  BarChart4,
+  Cable,
+  ChevronLeft,
+  Files,
+  Home,
+  ShoppingBag,
+  ShoppingBasket,
+  ShoppingCart,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Sidebar = () => {
   return (
     <div className="w-60 min-h-screen bg-slate-800 text-slate-50 justify-between ">
-            <div className="flex flex-col">
-                <div className="flex space-x-2 items-center bg-slate-950 py-3 px-2">
-                    <ShoppingCart />
-                    <span className='font-semibold text-2xl   '>Inventory</span>
-                </div>
+      <div className="flex flex-col">
+        <Link
+          href="#"
+          className="flex space-x-2 items-center bg-slate-950 py-3 px-2"
+        >
+          <ShoppingCart />
+          <span className="font-semibold text-2xl   ">Inventory</span>
+        </Link>
+      </div>
+      <nav className="flex flex-col gap-3 px-3 py-6 ">
+        <Link href="/" className="flex items-center space-x-2 bg-blue-600 text-slate-50 p-2 rounded-md">
+          <Home className="w-4 h-4" />
+          <span>Home</span>
+        </Link>
+        <button className="flex items-center space-x-2 p-2">
+          <BaggageClaim className="w-4 h-4" />
+          <span>Inventory</span>
+        </button>
 
-            </div>
-            <nav className='flex flex-col gap-4 p-2 '>
-                <Link href="/" className='flex items-center space-x-2'>
-                <Home className='w-4 h-4' />
-                <span>Home</span>
-                </Link>
-                <Link href="/" className='flex items-center space-x-2'>
-                <Home className='w-4 h-4' />
-                <span>Inventory</span>
-                </Link>
-               
-            </nav>
+        <button className="flex items-center space-x-2 p-2">
+          <ShoppingBasket className="w-4 h-4" />
+          <span>Sales</span>
+        </button>
 
-            <div className="flex flex-col">
-            <div className="flex space-x-2 items-center bg-slate-950 py-3 px-2">
-                    <ChevronLeft />
-                    
-                </div>
+        <button className="flex items-center space-x-2 p-2">
+          <ShoppingBag className="w-4 h-4" />
+          <span>Purchases</span>
+        </button>
 
-            </div>
+        <Link href="#" className="flex items-center space-x-2 p-2">
+          <Cable className="w-4 h-4" />
+          <span>Integrations</span>
+        </Link>
+
+        <Link href="#" className="flex items-center space-x-2 p-2">
+          <BarChart4 className="w-4 h-4" />
+          <span>Reports</span>
+        </Link>
+
+        <Link href="#" className="flex items-center space-x-2 p-2">
+          <Files className="w-4 h-4" />
+          <span>Documents</span>
+        </Link>
+      </nav>
+
+      <div className="flex flex-col">
+        <div className="flex space-x-2 items-center bg-slate-950 py-3 px-2">
+          <ChevronLeft />
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
