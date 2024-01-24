b2eaa9cd667261b70188e101995a199941ec4b94
Rajan <rajanj448@student.sfit.ac.in>
2024-01-24 09:08:55 +0530

    Working on the Dashboard Banner

--- app/(back-office)/dashboard/home/overview/page.jsx
+++ app/(back-office)/dashboard/home/overview/page.jsx
@@ -1,8 +1,10 @@
+import DashboardBanner from '@/components/dashboard/DashboardBanner'
 import React from 'react'
 
 const Dashboard = () => {
   return (
     <div>
+      <DashboardBanner />
         <h2>Dashboard</h2>
     </div>
   )
--- components/dashboard/HomeNavbar.jsx
+++ components/dashboard/HomeNavbar.jsx
@@ -1,52 +1,55 @@
 "use client";
-import { Building2 } from 'lucide-react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+import { Building2 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const HomeNavbar = () => {
-    const pathname=usePathname();
-    const navLinks=[
-        {
-            title:"Dashboard",
-            href:"/dashboard/home/overview"
-        },
-        {
-            title:"Getting Started",
-            href:"/dashboard/home/getting-started"
-        },
-        {
-            title:"Recent Updates",
-            href:"/dashboard/home/updates"
-        },
-        {
-            title:"Announcements",
-            href:"/dashboard/home/announcements"
-        },
-    ]
+  const pathname = usePathname();
+  const navLinks = [
+    {
+      title: "Dashboard",
+      href: "/dashboard/home/overview",
+    },
+    {
+      title: "Getting Started",
+      href: "/dashboard/home/getting-started",
+    },
+    {
+      title: "Recent Updates",
+      href: "/dashboard/home/updates",
+    },
+    {
+      title: "Announcements",
+      href: "/dashboard/home/announcements",
+    },
+  ];
   return (
-    <div className='h-32 p-5 header-bg bg-slate-50 border-b border-slate-200 '>
-        <div className="flex space-x-3">
-            <div className="flex w-12 h-12 rounded-lg bg-white items-center justify-center ">
-                <Building2 />
-            </div>
-            <div className="flex flex-col ">
-                <p className='text-slate-700 font-semibold'>Hello, Rajan Jha</p>
-                <span className='text-sm'>Mumbai</span>
-            </div>
+    <div className="h-32 p-5 header-bg bg-slate-50 border-b border-slate-200 ">
+      <div className="flex space-x-3">
+        <div className="flex w-12 h-12 rounded-lg bg-white items-center justify-center ">
+          <Building2 />
         </div>
-        <nav className='sticky mt-6 flex space-x-4 '>
-         {
-            navLinks.map((item,i)=>(
-                <Link key={i} href={item.href} className={`${pathname==item.href?"py-1 border-b-2 border-blue-600":"py-1"}`}>
-               {item.title}
-                </Link>
-            ))
-         }
-        </nav>
-       
+        <div className="flex flex-col ">
+          <p className="text-slate-700 font-semibold">Hello, Rajan Jha</p>
+          <span className="text-sm">Mumbai</span>
         </div>
-  )
-}
+      </div>
+      <nav className="sticky mt-6 flex space-x-4 ">
+        {navLinks.map((item, i) => (
+          <Link
+            key={i}
+            href={item.href}
+            className={`${
+              pathname == item.href ? "py-1 border-b-2 border-blue-600" : "py-1"
+            }`}
+          >
+            {item.title}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+};
 
-export default HomeNavbar
+export default HomeNavbar;
