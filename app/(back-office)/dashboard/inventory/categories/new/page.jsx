e7bedf2671decc62afa6be39f0583892dcab11df
Rajan <rajanj448@student.sfit.ac.in>
2024-01-25 10:29:09 +0530

    Creating Forms

--- app/(back-office)/dashboard/inventory/categories/new/page.jsx
+++ app/(back-office)/dashboard/inventory/categories/new/page.jsx
@@ -1,15 +1,60 @@
-import FormHeader from '@/components/dashboard/FormHeader'
-import { X } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import FormHeader from "@/components/dashboard/FormHeader";
+import { X } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const NewCategories = () => {
   return (
     <div>
-        <FormHeader title="New Category" href="#" />
-       
+      <FormHeader title="New Category" href="#" />
+      <div className="w-full max-w-3xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3">
+        <form className="">
+          <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label
+                for="name"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type product name"
+                required=""
+              />
+            </div>
+            <div className="w-full">
+              <label
+                for="brand"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Brand
+              </label>
+              <input
+                type="text"
+                name="brand"
+                id="brand"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Product brand"
+                required=""
+              />
+            </div>
+          </div>
+         <div className="mt-6 sm:col-span-1">
+         <button
+            type="submit"
+            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login to your account
+          </button>
+         </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewCategories
+export default NewCategories;
