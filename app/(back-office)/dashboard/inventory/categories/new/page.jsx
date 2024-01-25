2fbba0a33cb6e924ee270a2aba9e3e096dade68f
Rajan <rajanj448@student.sfit.ac.in>
2024-01-25 10:56:07 +0530

    Adding React Hook Form

--- app/(back-office)/dashboard/inventory/categories/new/page.jsx
+++ app/(back-office)/dashboard/inventory/categories/new/page.jsx
@@ -1,56 +1,60 @@
+"use client";
 import FormHeader from "@/components/dashboard/FormHeader";
 import { X } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const NewCategories = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <FormHeader title="New Category" href="#" />
       <div className="w-full max-w-3xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3">
-        <form className="">
+        <form onSubmit={handleSubmit(onSubmit)} className="">
           <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6">
             <div className="sm:col-span-2">
               <label
-                for="name"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="title"
+                className="block text-sm font-medium leading-6 text-gray-900 mb-2 "
               >
-                Name
+                Category Title
               </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Type product name"
-                required=""
-              />
-            </div>
-            <div className="w-full">
-              <label
-                for="brand"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Brand
-              </label>
-              <input
-                type="text"
-                name="brand"
-                id="brand"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Product brand"
-                required=""
-              />
+              <div className="mt-2">
+                <input
+                  {...register("title", { required: true })}
+                  type="text"
+                  name="title"
+                  id="title"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
+                  placeholder="Type the Category title"
+                />
+                {errors.title && (
+                  <span className="text-sm text-red-600 ">
+                    Category title is required
+                  </span>
+                )}
+              </div>
             </div>
           </div>
-         <div className="mt-6 sm:col-span-1">
-         <button
-            type="submit"
-            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Login to your account
-          </button>
-         </div>
+          <div className="mt-6 sm:col-span-1">
+            <button
+              type="submit"
+              className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+             Save
+            </button>
+          </div>
         </form>
       </div>
     </div>
